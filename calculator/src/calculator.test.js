e76891c2fe948c381b5f6ba2c1317853bd889e0d
Gabriel Schneider <gsstrozi@gmail.com>
2022-12-04 15:30:53 -0300

    Fix build actions

--- calculator/src/calculator.test.js
+++ calculator/src/calculator.test.js
@@ -2,9 +2,9 @@ import {sum, sub} from './calculator';
 
 describe('Calculator - Unit tests', () => {
     test('should return 10 when sum two values', () => {
-        const result = sum(5, 5);
+        const result = sum(6, 6);
 
-        expect(result).toBe(10);
+        expect(result).toBe(12);
     });
 
     test('should return 2 when subtract two values', () => {
